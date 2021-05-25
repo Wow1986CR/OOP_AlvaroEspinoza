aa4d2ec7f82990e777a4291a72771eb9eda324e8
Alvaro Espinoza <alvaro5218@gmail.com>
2021-05-24 19:46:11 -0600

    Trabajo en clase Leccion03

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,11 +1,13 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Alvaro;
+import com.ucreativa.familia.Lizbeth;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Alvaro comelon = new Alvaro();
+        Lizbeth saludando = new Lizbeth("Hola hola");
     }
 }
---
+++ src/com/ucreativa/familia/Lizbeth.java
@@ -0,0 +1,7 @@
+package com.ucreativa.familia;
+
+public class Lizbeth {
+    public Lizbeth (String saludo){
+        System.out.println(saludo);
+    }
+}
